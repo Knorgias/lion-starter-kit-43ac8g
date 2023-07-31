40717e8de2da929c36f6798a68db2747ea66d99b
Konstantinos Norgias <Konstantinos.Norgias@ing.com>
2023-07-31 15:12:22 +0200

    test lion-accordion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import './my-input';
 import '@lion/ui/define/lion-calendar.js';
-import '@lion/ui/define/lion-input.js';
-import '@lion/ui/define/lion-button.js';
 import '@lion/ui/define/lion-accordion.js';
 import { LitElement, html } from 'lit';
 
+import '@lion/ui/define/lion-button.js';
+import '@lion/ui/define/lion-accordion.js';
+
+
 class MyLionApp extends LitElement {
+
   render() {
     return html`
       <div style="text-align: center; margin: 0 auto; max-width: 700px">
         <h1>A Web App made with lion</h1>
-        <h1>Another title</h1>
-        <accordion>An accordion</accordion>
-        <button>Lion Button</button>
-        <h1>Another title</h1>
-        <h1>Another title</h1>
-        <h1>Another title</h1>
-        <h1>Another title</h1>
-        <my-input></my-input>
+
+        <lion-button>Default</lion-button>
+        <lion-accordion>
+          <h3 slot="invoker">
+            <button>Nutritional value</button>
+          </h3>
+          <p slot="content">
+            Orange flesh is 87% water, 12% carbohydrates, 1% protein, and contains negligible fat (table).
+            In a 100 gram reference amount, orange flesh provides 47 calories, and is a rich source of
+            vitamin C, providing 64% of the Daily Value. No other micronutrients are present in significant
+            amounts (table).
+          </p>
+        </lion-accordion>
         <lion-calendar></lion-calendar>
       </div>
     `;
